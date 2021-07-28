4188590c0e6b15f74ed8909c8de5e2058e5ee607
konikalily <konika.l.biswas@gmail.com>
2021-07-28 15:20:55 +0100

    exercise/B-shopping-list completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+  //Write your code here...
+  let peoplesShoppingList = document.querySelector("#content");
+  let contentList = document.createElement("ul");
+  arrayOfPeople.forEach((item) => {
+    let listItem = document.createElement("li");
+    listItem.innerText = item;
+    contentList.appendChild(listItem);
+  });
+  peoplesShoppingList.append(contentList);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
