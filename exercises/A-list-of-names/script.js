040536c308e481925688765d0dd9b256e8dcea14
konikalily <konika.l.biswas@gmail.com>
2021-07-28 13:34:30 +0100

    Exercise folder-A-list-of names completed

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,11 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  //looping through array
+  for (let person of arrayOfPeople) {
+    content.appendChild(document.createElement("h1")).innerHTML = person.name;
+    content.appendChild(document.createElement("h2")).innerHTML=  person.job;
+  };
+
 }
 
 let people = [
